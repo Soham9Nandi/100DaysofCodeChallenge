e0c4b6bf389b536ae7b686e3c1fae1c550069b43
Soham9Nandi <61171273+Soham9Nandi@users.noreply.github.com>
2023-06-13 22:41:48 +0530

    Add files via upload

--- Leetcode testing.py
+++ Leetcode testing.py
@@ -1,14 +1,25 @@
 class Solution(object):
-    def intersection(self, nums1, nums2):
+    def minimumOperations(self, nums):
         """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :rtype: List[int]
+        :type nums: List[int]
+        :rtype: int
         """
-        nums1.sort()
-        nums2.sort()
-        n1 = set(nums1)
-        n2 = set(nums2)
-        return n1.intersection(n2)
+        flag = 0
+        while nums.count(0) != len(nums):
+            s = list(set(nums))
+            s.sort()
+            if min(s) == 0:
+                if len(s) > 1:
+                    sub = s[s.index(min(s)) + 1]
+                    for i in range(0, len(nums)):
+                        if nums[i] != 0:
+                            nums[i] -= sub
+                    flag += 1
+            else:
+                sub = s[s.index(min(s))]
+                for i in range(0, len(nums)):
+                    if nums[i] != 0:
+                        nums[i] -= sub
+                flag += 1
 
-        return res
+        return flag
